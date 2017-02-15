a5b8f8c8ef670d2b5e9ecf7e6da7c049be1b815d
Kong Hyeontaek <rotanev7@gmail.com>
2017-02-15 15:12:38 +0900

    initial commit

---
+++ swe/settings/prod.py
@@ -0,0 +1,18 @@
+from .settings import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['*']
+
+STATIC_ROOT = os.path.join(BASE_DIR, "..", "staticfiles")
+MEDIA_ROOT = os.path.join(BASE_DIR, "..", "media")
+
+DATABASES = {
+    'default': {
+        'ENGINE': os.environ.get('DJANGO_DATABASE_ENGINE', 'django.db.backends.postgresql_psycopg2'),
+        'NAME': os.environ.get('DJANGO_DATABASE_NAME', 'ubuntu'),
+        'USER': os.environ.get('DJANGO_DATABASE_USER', 'ubuntu'),
+        'PASSWORD': os.environ.get('DJANGO_DATABASE_PASSWORD', 'bhunjimko123'),
+        'HOST': os.environ.get('DJANGO_DATABASE_HOST', '127.0.0.1'),
+    }
+}
